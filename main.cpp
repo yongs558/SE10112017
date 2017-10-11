d2918774368f76d46b850abb210110ea80d71d5a
unknown <presentation@LAB1017.student.domain>
2017-10-11 12:24:47 +0800

    adding main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,11 @@ using namespace std;
 
 int main(){
 	cout << "Hello World\n";
+<<<<<<< HEAD
 	cout << "making new branch\n";
+=======
+	cout << "Blah Blah bLah\n";
+	cout << "making more branch\n";
+>>>>>>> hotfix
 	return 0;
 }
